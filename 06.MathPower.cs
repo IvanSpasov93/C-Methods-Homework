43c044977825f769da8ab3bfd189f163848fc84b
IvanSpasov93 <northmc93@gmail.com>
2017-01-31 12:34:11 +0200

    06.MathPower

---
+++ 06.MathPower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.MathPower
+{
+    class Program
+    {
+        static double RaiseToPower(double number, double power)
+        {
+            double result = Math.Pow(number,power);
+            
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            double result = RaiseToPower(number, power);
+            Console.WriteLine(result);
+        }
+    }
+}
